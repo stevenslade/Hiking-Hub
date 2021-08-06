09d0129df75e88dcde3dd656f5a3d5c1de76f32e
Ben S <benslinde@gmail.com>
2021-08-06 09:22:58 -0400

    added tailwind formatting for reddit fetched items

--- assets/js/reddit.js
+++ assets/js/reddit.js
@@ -7,12 +7,12 @@
 //keyWord
 //sortBy
 
-function createAppendReddit () {
+function createAppendReddit (image, title, description, link) {
   // make variables of dataset items - currently hardcoded placeholders
-  var image = "https://cdn.vox-cdn.com/thumbor/SfU1irp-V79tbpVNmeW1N6PwWpI=/0x0:640x427/1200x800/filters:focal(0x0:640x427)/cdn.vox-cdn.com/uploads/chorus_image/image/45970810/reddit_logo_640.0.jpg";
-  var title = "This will be a Title";
-  var description = "People are going to be talking about how great the title above is";
-  var link = "https://www.reddit.com/r/whatsthissnake/comments/o76zo7/2_snakes_seen_on_a_trail_on_cochran_shoals/";
+  // var image = "https://cdn.vox-cdn.com/thumbor/SfU1irp-V79tbpVNmeW1N6PwWpI=/0x0:640x427/1200x800/filters:focal(0x0:640x427)/cdn.vox-cdn.com/uploads/chorus_image/image/45970810/reddit_logo_640.0.jpg";
+  // var title = "This will be a Title";
+  // var description = "People are going to be talking about how great the title above is";
+  // var link = "https://www.reddit.com/r/whatsthissnake/comments/o76zo7/2_snakes_seen_on_a_trail_on_cochran_shoals/";
 
   //create the container and card elements
   var redditContainer = document.createElement("div");
@@ -53,13 +53,13 @@ function createAppendReddit () {
   //element.classList.add("someclass - defined in your css") will add a new class to the existing 
   //redditContainer.setAttribute('class', "redditContainer");
   //This formatting is currently all done in Tailwind
-  redditContainer.setAttribute('class', "max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl");
+  redditContainer.setAttribute('class', "max-w-2xl mx-auto bg-blue-300 bg-opacity-60 rounded-xl shadow-md overflow-hidden m-10 border-2 border-green-500");//max-w-md
   redditCard.setAttribute('class', "md:flex");
   redditImageCard.setAttribute('class', "md:flex-shrink-0");
-  redditImage.setAttribute('class', "w-36 object-cover"); //md:h-full md:w-48 removed this responsive content, also was h-full w-48
-  //redditDescCard.setAttribute('class', "p-8");
-  redditCardTitle.setAttribute('class', "uppercase tracking-wide text-sm text-indigo-500 font-semibold");
-  redditDescription.setAttribute('class', "mt-2 text-gray-500");
+  redditImage.setAttribute('class', "h-36 w-36 object-cover"); //md:h-full md:w-48 removed this responsive content, also was h-full w-48
+  redditDescCard.setAttribute('class', "mx-4")
+  redditCardTitle.setAttribute('class', "uppercase tracking-wide text-lg text-black font-bold");
+  redditDescription.setAttribute('class', "mt-2 text-md text-black font-medium my-2");
   redditLink.setAttribute('class',"block mt-1 text-lg leading-tight font-medium text-black hover:underline");
 
   //attach the container to the HTML anchor point "reddit"
@@ -105,39 +105,41 @@ fetch(redditQueryUrl)
     return res.json();   // Convert the data into JSON
   })
   .then(function(data) {
-    //console.log(data.data.children);   // Logs the data to the console already cutting it down with the .data.children parameters
-    var title = data.data.children[0].data.title;
-    console.log("title: ", title);
-    //More amazingness, some reddit urls are links to posts, some are links to images, its not reliable we need to use the permalink data
-    //console.log("url: ", data.data.children[0].data.url);
-    //So permalink should give an address fragment to which we can attach the prefix https://www.reddit.com and then get a useable link for each item
-    var permalinkHttp = "https://www.reddit.com" + data.data.children[0].data.permalink
-    console.log("permalink: ", permalinkHttp);
-    var description = data.data.children[0].data.selftext; 
-    console.log("Description: ", description);
-    //Thumbnail can be an image or a self reference it depends on how the user posted, it's not reliable enough for image population
-    //console.log("Thumbnail: " + data.data.children[0].data.thumbnail);
+    for(var i =0; i < data.data.children.length; i++) {
+      //console.log(data.data.children);
+      //console.log(data.data.children);   // Logs the data to the console already cutting it down with the .data.children parameters
+      var title = data.data.children[i].data.title;
+      //console.log("title: ", title);
+      //More amazingness, some reddit urls are links to posts, some are links to images, its not reliable we need to use the permalink data
+      //console.log("url: ", data.data.children[0].data.url);
+      //So permalink should give an address fragment to which we can attach the prefix https://www.reddit.com and then get a useable link for each item
+      var permalinkHttp = "https://www.reddit.com" + data.data.children[i].data.permalink
+      //console.log("permalink: ", permalinkHttp);
+      var description = data.data.children[i].data.selftext; 
+      //console.log("Description: ", description);
+      //Thumbnail can be an image or a self reference it depends on how the user posted, it's not reliable enough for image population
+      //console.log("Thumbnail: " + data.data.children[0].data.thumbnail);
 
-    //Some posts do not have an image which leaves the preview parameter empty so we only get the thumbnail image if it exists
-    if(data.data.children[0].data.preview) {
-    //And of course it has to be encoded because reddit so we can't use it unless we decoded it.  If we try to follow it we get a 403 error.
-    var imageUrlEncoded = data.data.children[0].data.preview.images[0].source.url;
-    console.log("imageUrlEncoded: ", imageUrlEncoded);
-    //in order to decode we need to replace "amp;s" with "s"
-    var imageUrlDecoded = imageUrlEncoded.replace("amp;s", "s");
-    //This url can now be used as a link to display an image
-    console.log("imageUrlDecoded: ", imageUrlDecoded);
-    } else {
-      var imageUrlDecoded = "https://cdn.vox-cdn.com/thumbor/SfU1irp-V79tbpVNmeW1N6PwWpI=/0x0:640x427/1200x800/filters:focal(0x0:640x427)/cdn.vox-cdn.com/uploads/chorus_image/image/45970810/reddit_logo_640.0.jpg";
-      console.log("imageUrlDecoded: ", imageUrlDecoded);
+      //Some posts do not have an image which leaves the preview parameter empty so we only get the thumbnail image if it exists
+      if(data.data.children[i].data.preview) {
+        //And of course it has to be encoded because reddit so we can't use it unless we decoded it.  If we try to follow it we get a 403 error.
+        var imageUrlEncoded = data.data.children[i].data.preview.images[0].source.url;
+        //console.log("imageUrlEncoded: ", imageUrlEncoded);
+        //in order to decode we need to replace "amp;s" with "s"
+        var imageUrlDecoded = imageUrlEncoded.replace("amp;s", "s");
+        //This url can now be used as a link to display an image
+        //console.log("imageUrlDecoded: ", imageUrlDecoded);
+      } else {
+        var imageUrlDecoded = "https://cdn.vox-cdn.com/thumbor/SfU1irp-V79tbpVNmeW1N6PwWpI=/0x0:640x427/1200x800/filters:focal(0x0:640x427)/cdn.vox-cdn.com/uploads/chorus_image/image/45970810/reddit_logo_640.0.jpg";
+        console.log("imageUrlDecoded: ", imageUrlDecoded);
+     }
+     //After fetching from the api endpoints, run the function to create and append the data
+     createAppendReddit (imageUrlDecoded, title, description, permalinkHttp);
     }
   })
   .catch(function(err) {
     console.log(err);   // Log error if any
   });
-
-  //After fetching from the api endpoints, run the function to create and append the data
-  createAppendReddit ()
 }
 
 //calls initial functions
